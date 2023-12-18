e82f8a803a06942f5db2eac2132f9223a53af363
Denis Costello <G00396309@atu.ie>
2023-12-18 00:05:23 +0000

    First commit

---
+++ src/main/java/ie/atu/addfoodservice/AddFoodServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.addfoodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddFoodServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddFoodServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/addfoodservice/AddFoodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.addfoodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddFoodServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
