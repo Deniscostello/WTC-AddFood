d71ad91b04d231f1d55ac8f441ca387e94c30561
Denis Costello <G00396309@atu.ie>
2024-04-08 11:24:22 +0100

    Updated to align with new security, food adds by userId

--- src/main/java/ie/atu/addfoodservice/FoodRepository.java
+++ src/main/java/ie/atu/addfoodservice/FoodRepository.java
@@ -4,6 +4,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface FoodRepository extends MongoRepository<InputFood, ObjectId> {
+public interface FoodRepository extends MongoRepository<InputFood, String> {
+    Optional<InputFood> findByUserId(String userId);
 }
